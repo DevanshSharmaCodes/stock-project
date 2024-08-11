c68f3ff2897d8cb7e789be1262f5d23c3e11abe5
DevanshSharmaCodes <devanshmkv2003@gmail.com>
2024-08-12 00:01:41 +0530

    Created sidebar on frontend

---
+++ client/src/app/dashboard/page.jsx
@@ -0,0 +1,21 @@
+"use client"
+import React from 'react'
+import SideBar from '../components/Sidebar'
+
+
+const Dashboard = () => {
+  return (
+    <div className='h-screen flex'>
+        <div className='w-1/5'>
+            <SideBar />
+        </div>
+        <div className='w-4/5'>
+            <div>
+                {/* <StockChart /> */}
+            </div> 
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
