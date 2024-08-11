bbb06cdbe3e236b57c303c92b64546b7d7ea60a8
DevanshSharmaCodes <devanshmkv2003@gmail.com>
2024-08-11 22:19:45 +0530

    Initial Commit

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import AnalyticsReport from "./components/AnalyticsReport";
+
+export default function Home() {
+  return (
+    <div>
+      <AnalyticsReport/>
+      Stock broker app
+    </div>
+  );
+}
